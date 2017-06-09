96d1d69d68f11a23756b73bdfc2d005472aface0
Misha.19 <nomad24000@gmail.com>
2017-06-09 13:17:41 +0300

    Added scroll

--- app/src/main/java/com/example/myapplication/Shop.java
+++ app/src/main/java/com/example/myapplication/Shop.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Shop extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class Shop extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+
+
+
+        final long[] mon = {100000};
+
+        Button button = (Button) findViewById(R.id.button1);
+        final Shop this_ = this;
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this_, Shop.class);
+                mon[0] -=  100;
+            }
+        };
+        button.setOnClickListener(listener);
+
     }
 }
