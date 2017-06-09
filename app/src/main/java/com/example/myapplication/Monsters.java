f0d6d035801e4edae0ace35a0a37baa3f34ca97b
kononovk <kononov.nikolay.nk1@gmail.com>
2017-06-09 13:31:02 +0300

    Monstrik added

--- app/src/main/java/com/example/myapplication/Monsters.java
+++ app/src/main/java/com/example/myapplication/Monsters.java
@@ -1,8 +1,28 @@
 package com.example.myapplication;
+import android.widget.ImageView;
 
 /**
  * Created by Kononov Nickolay
  */
 
-public class Monsters {
+public class Monsters
+{
+    protected int hp;
+    protected int gold;
+    ImageView Image;
+
+    int GetHp()
+    {
+        return hp;
+    }
+    int GetGold()
+    {
+        return gold;
+    }
+    ImageView GetImg()
+    {
+        return Image;
+    }
 }
+
+
