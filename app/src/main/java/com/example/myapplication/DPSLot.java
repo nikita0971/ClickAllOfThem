0dadeaa197d0e339abced1140965121cf90a9948
Nikita <nikitoskin.2012@gmail.com>
2017-06-14 10:31:56 +0300

    add missing files

---
+++ app/src/main/java/com/example/myapplication/DPSLot.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import android.widget.Button;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.pow;
+
+public class DPSLot extends Lot {
+
+    private double ImprovementBonus;
+    private int HeroUnlockOrder;
+    private double PurchaseCost;
+    private Button button;
+
+    public double getImprovementBonus() {
+
+        return ImprovementBonus;
+    }
+
+    public int getHeroUnlockOrder() {
+        return HeroUnlockOrder;
+    }
+
+    public double getPurchaseCost() {
+        return PurchaseCost;
+    }
+
+    public double getCost(double curentLevel){
+        return PurchaseCost / 10 * pow(1.082, getCurentLevel());
+    }
+
+    public void creatLot(){
+
+    }
+
+    public void buyLot( double Credits ) {
+        ImprovementBonus = (double)(((int)getCurentLevel() / 10) + 1);
+        MainActivity.Credits -= getCost(getCurentLevel());
+        PurchaseCost = getCost(getCurentLevel());
+        HeroUnlockOrder++;
+        MainActivity.DPS = PurchaseCost / 10 * pow(1 - 23 / 1000 * min(HeroUnlockOrder, 34), min(HeroUnlockOrder, 34)) * ImprovementBonus;
+    }
+
+
+}
